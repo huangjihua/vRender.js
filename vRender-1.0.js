9a7b2db5be59898a76ea753f53d3c9599c48312a
888dazhuang <888dazhuang@163.com>
2016-01-21 17:07:18 +0800

    Update vRender-1.0.js

--- vRender-1.0.js
+++ vRender-1.0.js
@@ -1,4 +1,4 @@
- (function (e) {
+(function (e) {
         e.$ || (e.$ = {});
 
         function _createpType(value, dataType, msg) {
@@ -28,7 +28,7 @@
             else {
                 if (dataType) {
                     dataType = dataType.replace(_rdkh, "");
-                    //小括号中的如果是数字类型，就执行截取字符串操作。
+
                     if (_num.test(dataType) === true) {
                         if (1 * dataType > 0) {
                             value = value.substring(0, 1 * dataType) + "...";
@@ -37,13 +37,13 @@
                             value = "*" + value.substring(-1 * dataType, value.length);
                         }
                     } else {
-                    	//小括号中的内容，判断是否含有此全局函数，有就调用
+
                         if (typeof (window[dataType]) == "function") {
-                        	//value是JSON中的结果
+
                             return window[dataType](value);
                         }
                         else if (typeof (window[dataType.replace("obj_", "")]) == "function") {
-                        	//msg是value的父级
+
                             return window[dataType.replace("obj_", "")](msg);
                         }
                         if (/(y+)/.test(dataType)){
@@ -65,16 +65,17 @@
             var time = new Date(value);
             if (time != "Invalid Date") {
             var o = {
-                "M+": time.getMonth() + 1,                 //月份
-                "d+": time.getDate(),                    //日
-                "h+": time.getHours(),                   //小时
-                "m+": time.getMinutes(),                 //分
-                "s+": time.getSeconds(),                 //秒
-                "q+": Math.floor((time.getMonth() + 3) / 3), //季度
-                "S": time.getMilliseconds()             //毫秒
+                "M+": time.getMonth() + 1,
+                "d+": time.getDate(),
+                "h+": time.getHours(),
+                "m+": time.getMinutes(),
+                "s+": time.getSeconds(),
+                "q+": Math.floor((time.getMonth() + 3) / 3),
+                "S": time.getMilliseconds()
             };
 
                 fmt = fmt.replace(RegExp.$1, (time.getFullYear() + "").substr(4 - RegExp.$1.length));
+
                 for (var k in o)
                     if (new RegExp("(" + k + ")").test(fmt))
                         fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
@@ -86,7 +87,7 @@
         function _createValue(columnValue, data, status) {
             var nodata = "";
             var value = data[columnValue]|| (typeof(data[columnValue])=="number"?data[columnValue]:"");
-	    /////根据obj.x.x.x.x.x.x.读取相应JSON中的数据
+
             if (columnValue.indexOf("||") > -1) {
                 nodata = columnValue.split("||")[1];
                 value = data[columnValue.split("||")[0]]
@@ -119,27 +120,27 @@
 
             return value||nodata;
         }
-        var _getText = "[^\\s(}})({{)]+";//读取{{text}}中的text
-        var _getChild = "\\[child[0-9]{0,1}\\]";//读取{{text[child]}}中的child
-        var _getList = "\\[list[0-9]{0,1}\\]";//读取{{text[list]}}中的list
-        var _getType = "\(\\([^\\s]+\\)\){0,1}";//读取{{text(time)}}中的time
-        var _getTypeV = "\(\\([^\\s]+\\)\)";//读取{{text(time)}}中的time,同上，应用在一定含有(time)的场景
+        var _getText = "[^\\n(}})({{)]+";
+        var _getChild = "\\[child[0-9]{0,1}\\]";
+        var _getList = "\\[list[0-9]{0,1}\\]";
+        var _getType = "\(\\([^\\n]+\\)\){0,1}";
+        var _getTypeV = "\(\\([^\\n]+\\)\)";
         
-        var _getStatus = "\({[^\\s]+}\){0,1}";//读取{{text{0:123}}}中的{0:123}
-        var _getStatusV = "\({[^\\s{}]+[:,]+[^\\s{}]+}\){1}";//读取{{text{0:123}}}中的{0:123}，应用在一定含有(time)的场景
+        var _getStatus = "\({[^\\n]+}\){0,1}";
+        var _getStatusV = "\({[^\\n{}]+[:,]+[^\\n{}]+}\){1}";
         
         
-        var _reText = new RegExp(_getStatusV + "|\([{]{2}|[}]{2}\)|" + _getChild + "|" + _getList + "|" + _getType, "g");//获取所有{{xx[xx](xxx){x:xxx}}}
-        var _canRe = new RegExp("[{]{2}|[}]{2}|" + _getChild + "|\\([^\\s]+\\)}}", "g");
+        var _reText = new RegExp(_getStatusV + "|\([{]{2}|[}]{2}\)|" + _getChild + "|" + _getList + "|" + _getType, "g");
+        var _canRe = new RegExp("[{]{2}|[}]{2}|" + _getChild + "|\\([^\\n]+\\)}}", "g");
         var _getAllChild = new RegExp("{{" + _getText + _getChild + _getType + _getStatus + "}}", "g");
         var _getAllList = new RegExp("{{" + _getText + _getList + _getType + _getStatus + "}}", "g");
         var _getAll = new RegExp("{{" + _getText + _getType + _getStatus + "}}", "g");
         var _anNum2 = new RegExp("[^\\(\\)]+"), _num = new RegExp("^-{0,1}[0-9]+$");
 
-        function vCreateChild(data,_attr,str,level){//参数data:json数据，参数_attr,当前属性的名字，参数str:html字符串
+        function vCreateChild(data,_attr,str,level){
             var msg=data[_attr]||[];
             
-            //截取[list] [end]之间的内容
+
             var childName = "{{" + _attr + "[list" + level + "]}}", childEnd = "{{" + _attr + "[end" + level + "]}}";
             var childStart = str.indexOf(childName);
             var childend = str.indexOf(childEnd);
@@ -152,12 +153,12 @@
                 var _getNowChild = new RegExp("{{" + _getText + "\\[child" + level + "\\]" + _getType + _getStatus + "}}", "g");
                 for (var i = 0; i < l; i++) {
                     var tmp = regstr;
-                    //循环至{{xxx[](){}}}格式内容全部被渲染完成
+
                     while (tmp.match(_getNowChild)) {
                         var pstr = tmp.match(_getNowChild);
-                        var text = pstr[0].replace(_reText, "");//获取去{{text[](){}}}中的text
-                        var pType = pstr[0].match(_getTypeV);//获取去{{text[](){}}}中的()
-                        var status = pstr[0].match(_getStatusV);//获取去{{text[](){}}}中的{}
+                        var text = pstr[0].replace(_reText, "");
+                        var pType = pstr[0].match(_getTypeV);
+                        var status = pstr[0].match(_getStatusV);
                         if (msg[i].constructor ==String){
                            tmp = tmp.replace(pstr[0],msg[i], pType, msg[i])
                         }
@@ -174,7 +175,7 @@
             }
         }
 
-        function createByLevel(data,tempStr,level){//参数data：json数据，参数tempStr:html内容（字符串）,参数level:渲染级别
+        function createByLevel(data,tempStr,level){
             var _getNowList = new RegExp("{{" + _getText + "\\[list" + level + "\\]" + _getType + _getStatus + "}}", "g");
 
             var lkey = tempStr.match(_getNowList);
@@ -185,7 +186,7 @@
                 return tempStr;
             }
         }
-        function _BaseRanderAppend(data, _tempStr,dl) {//参数data：json数据，参数_tempStr:html内容（字符串）
+        function _BaseRanderAppend(data, _tempStr,dl) {
 		dl =dl?dl:data.length;
             var str = "";
             for (var j = 0; j < dl; j++) {
@@ -199,7 +200,8 @@
             return str;
         }
 
-        function _angular(str, msg) {//这个函数是最后执行的，渲染非[list]的内容，且清除所有多余{{}}
+        function _angular(str, msg) {
+            console.log(str.match(_getAll))
             while (str.match(_getAll)) {
                 var pstr = str.match(_getAll);
                 var text = pstr[0].replace(_reText, "");
@@ -216,16 +218,14 @@
             }
             return window["v" + element];
         }
-        e.$.vRender = function (element, date, config) {
-            if (date.constructor != Array) {
-                date = [date];
-            }
-            config || (config = {});//将来扩展配置用，暂时没用
-            var strt = _createThisEle(element);//将html保存到全局变量，下次调用直接从全局变量获取
-            if(config["append"]){
-            	document.getElementById(element).innerHTML+=_BaseRanderAppend(date,strt);
-            }else{
-            	document.getElementById(element).innerHTML=_BaseRanderAppend(date,strt)
-            }
+    e.$.vRender=function(element,date,config){
+        if(date.constructor!=Array){date=[date]}
+        config||(config={});
+        var strt=_createThisEle(element);
+        if(config["append"]){
+            document.getElementById(element).innerHTML+=_BaseRanderAppend(date,strt);
+        }else{
+            document.getElementById(element).innerHTML=_BaseRanderAppend(date,strt)
         }
+    }
     })(window)
